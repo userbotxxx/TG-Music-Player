d3eec73995380296053b5a331235629e1623dc8e
userbotxxx <88811651+userbotxxx@users.noreply.github.com>
2021-08-17 17:17:44 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -23,10 +23,10 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/HiTechRockets"
+                        "Group", url="https://t.me/SnaB_HelP"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/HiTechRocket"
+                        "Channel", url="https://t.me/iNfO_Maker"
                     )
                 ]
             ]
